7dd50cb46d42827c5565e288e440a204bb26ff55
ODHL-dev <ourbadalyo@gmail.com>
2025-12-28 17:56:46 +0000

    Vuforia C#
    Ce code permet de toggle l'AR caméra de Vuforia

---
+++ ArCamControl.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Vuforia;
+
+public class ArCamControl : MonoBehaviour
+{
+    private VuforiaBehaviour arCam;
+    void Start()
+    {
+        arCam = GetComponent<VuforiaBehaviour>();
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            if (arCam != null)
+            {
+                arCam.enabled = !arCam.enabled;
+            }
+        }
+        
+    }
+}
